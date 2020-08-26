097a9ada00808e5b08589f0b315870e257a33890
Carlos Machado <carlosedumachado@edu.univali.br>
2020-08-26 11:07:15 -0300

    Refactoring of PasswordGenerator class

--- src/site/negocio/carlosmachado/core/PasswordGenerator.java
+++ src/site/negocio/carlosmachado/core/PasswordGenerator.java
@@ -5,10 +5,10 @@ import java.util.Random;
 public class PasswordGenerator {
 
     private static final Random rand = new Random(System.currentTimeMillis());
-    
+
     public static String defaultPass() {
         String password = "";
-        
+
         password += uppercase();
         password += lowercase();
         password += lowercase();
@@ -17,54 +17,56 @@ public class PasswordGenerator {
         password += number();
         password += number();
         password += number();
-        
+
         return password;
     }
-    
-    public static String customPass(int lenght, boolean lowercase, boolean number, boolean symbol, boolean uppercase) {
+
+    public static String customPass(
+            int lenght, boolean lowercase, boolean number,
+                        boolean symbol, boolean uppercase) {
+        String lowerChars = "abcdefghijklmnopqrstuvwxyz";
+        String symbolChars = "@#$%{}[]()/~,;:.<>";
+
         String password = "";
-        for (int i = 0; i < lenght; i++) {
-            boolean willChoice = false;
-            while (!willChoice) {
-                int selected = rand.nextInt(4);
-                if (lowercase && selected == 0) {
-                    password += lowercase();
-                    willChoice = true;
-                }
-                if (number && selected == 1) {
-                    password += number();
-                    willChoice = true;
-                }
-                if (symbol && selected == 2) {
-                    password += symbol();
-                    willChoice = true;
-                }
-                if (uppercase && selected == 3) {
-                    password += uppercase();
-                    willChoice = true;
-                }
+        String characters = "";
+
+        if (lowercase) {
+            characters += lowerChars;
+        }
+        if (uppercase) {
+            characters += lowerChars.toUpperCase();
+        }
+        if (symbol) {
+            characters += symbolChars;
+        }
+        if (number) {
+            for(int i = 0; i < 10; i++){
+                characters += Integer.toString(i);
             }
+        }
+
+        for (int i = 0; i < lenght; i++) {
+            password += characters.charAt(rand.nextInt(characters.length()));
         }
         return password;
     }
 
     public static char lowercase() {
-        char[] c = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-        return c[rand.nextInt(c.length)];
+        String c = "abcdefghijklmnopqrstuvwxyz";
+        return c.charAt(rand.nextInt(c.length()));
     }
 
     public static char uppercase() {
-        char[] c = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-        return Character.toUpperCase(c[rand.nextInt(c.length)]);
+        return Character.toUpperCase(lowercase());
     }
 
     public static char symbol() {
-        char[] c = { '@', '#', '$', '%', '{', '}', '[', ']', '(', ')', '/', '~', ',', ';', ':', '.', '<', '>' };
-        return c[rand.nextInt(c.length)];
+        String c = "@#$%{}[]()/~,;:.<>";
+        return c.charAt(rand.nextInt(c.length()));
     }
 
     public static char number() {
         return Integer.toString(rand.nextInt(10)).charAt(0);
     }
-    
+
 }
